31e3769d6a5e31146a9fd9ab6eda37b65f3ecf1f
Brian Gilham <me@briangilham.com>
2016-02-18 22:38:21 -0500

    Initial commit.

---
+++ extension.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $("#pull_request_body").load("https://gist.githubusercontent.com/bgilham/906ba54fedddd81a009b/raw/4a7ae027b7e58dc211de9c365c8411f393c9779d/example.txt");
+});
